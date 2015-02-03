4fbbc3693397c3265d3290826cfe7512dac93acc
Victor <viclee02@gmail.com>
2015-02-02 15:41:57 -1000

    pass food

--- zombies.js
+++ zombies.js
@@ -31,17 +31,15 @@ function Weapon (name, damage) {
   Item.call(this, name);
 }
 
+/**
+ * Weapon Extends Item Class
+ * -----------------------------
+ */
 Weapon.prototype = Object.create(Item.prototype,{
   constructor : {
     value : Item
   }
 });
-
-/**
- * Weapon Extends Item Class
- * -----------------------------
- */
-
 
 
 /**
@@ -60,11 +58,22 @@ Weapon.prototype = Object.create(Item.prototype,{
  * @property {number} energy
  */
 
+function Food (name, energy){
+  this.energy = energy;
+
+  Item.call(this, name);
+}
 
 /**
  * Food Extends Item Class
  * -----------------------------
  */
+
+ Food.prototype = Object.create(Item.prototype,{
+  constructor : {
+    value : Item
+  }
+ });
 
 
 
